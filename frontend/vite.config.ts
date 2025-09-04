fad76a0ae6e4262fdf083ee7a3f0c6fa48b34414
Koushal kumar <kushwahakoushal78@gmail.com>
2025-09-04 23:01:31 +0530

    frontend deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://resume-analyser-0hmh.onrender.com",
         changeOrigin: true,
         secure: false,
       }
